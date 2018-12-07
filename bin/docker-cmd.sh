659b79e86202f29e8201ebbd767f20f6f1a7d77a
Chris Wells <chris@cevanwells.com>
2018-12-06 21:57:05 -0500

    Fixed error in command (missing script dir)

--- bin/docker-cmd.sh
+++ bin/docker-cmd.sh
@@ -6,4 +6,4 @@ JAVA_DIR='/usr/bin'
 PROFILE="${1}"
 SCRIPT="${2}"
 
-$JAVA_DIR/java $SQLWORKBENCH_ARGS -profile=$PROFILE -script="${SCRIPT}.sql" 2>&1
+$JAVA_DIR/java $SQLWORKBENCH_ARGS -profile=$PROFILE -script="/app/sql/${SCRIPT}.sql" 2>&1
